2255bac9dd2423e138fa30bb102dabada3d0f6c7
riyadfirdaus <56018683+riyadfirdaus@users.noreply.github.com>
2021-06-14 16:45:14 +0700

    Edit Front-End

--- resources/views/detail-paket.blade.php
+++ resources/views/detail-paket.blade.php
@@ -1,5 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>TEST</h1>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <div class="row">
+                <div class="col">
+                    <h5>Status</h5>
+                    <h6>Bisa diambil</h6>
+                </div>
+                <div class="d-flex flex-col-reverse pr-3">
+                    <a class="button" href="edit-paket">Edit paket</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4">No Resi</div>
+                    <div class="col-1">:</div>
+                    <div class="col">12345678A</div>
+                </div>
+                <div class="row">
+                    <div class="col-4">Nama Barang</div>
+                    <div class="col-1">:</div>
+                    <div class="col">Makanan Kucing</div>
+                </div>
+                <div class="row">
+                    <div class="col-4">Deskripsi</div>
+                    <div class="col-1">:</div>
+                    <div class="col">
+                        12345678A
+                        12345678A
+                        12345678A
+                        12345678A
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
--- resources/views/tambah-paket.blade.php
+++ resources/views/tambah-paket.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-@extends('layouts.app')
-
-@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -13,11 +10,26 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
+
+                        <div class="form-group row">
+                            <label for="resi" class="col-md-4 col-form-label text-md-right">No Resi</label>
+
+                            <div class="col-md-6">
+                                <input id="resi" type="text" class="form-control @error('resi') is-invalid @enderror" name="resi" value="{{ old('resi') }}" required autocomplete="resi" autofocus>
+
+                                @error('resi')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">Nama Barang</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name">
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -28,45 +40,17 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="deskripsi" class="col-md-4 col-form-label text-md-right">Deskripsi</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <textarea class="form-control" id="edeskripsi" rows="3"></textarea>
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    Tambah
                                 </button>
                             </div>
                         </div>
@@ -76,6 +60,4 @@
         </div>
     </div>
 </div>
-@endsection
-
 @endsection
